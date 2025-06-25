bbee8dbd7c9493287394551a0964ae58978efadf
Theophila <stheophilamurphy@gmail.com>
2025-06-25 17:11:07 +0530

    Commit message

---
+++ DAY-12_Activity_error.py
@@ -0,0 +1,21 @@
+
+try:
+    numerator = 10
+    denominator = 0
+    result = numerator / denominator
+    print (f" Result :{result}")
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed")
+finally:
+    print("Execution completed Denominator is Zero.")
+
+
+try:
+    numerator = 0
+    denominator = 5
+    result = numerator / denominator
+    print(f"Result: {result}")
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+finally:
+    print("Execution completed (Numerator is 0).")
